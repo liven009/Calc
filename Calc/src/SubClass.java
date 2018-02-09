8bd7884f15b734b5c6ee37eb08da75b3b547b0e6
USER <USER@COM141-152>
2018-02-09 11:07:40 +0900

    서브클래스 수정

--- Calc/src/SubClass.java
+++ Calc/src/SubClass.java
@@ -4,7 +4,7 @@ public class SubClass implements Hab{
 	public void Calc(NumVo nc) {
 		
 		// TODO Auto-generated method stub
-		System.out.println("빼기");
+		System.out.println("빼기!!!!");
 		int num1 = nc.getNum1();
 		int num2 = nc.getNum2();
 		System.out.println(num1+"-"+num2+"="+(num1-num2));
